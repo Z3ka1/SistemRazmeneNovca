a0214ea06b0534dd02d3afdd12104cdc29587ec6
Marko Ankić <120253601+Z3ka1@users.noreply.github.com>
2023-12-27 19:45:56 +0100

    Dodavanje kartice u bazu (Engine)

--- Engine/app.py
+++ Engine/app.py
@@ -48,6 +48,37 @@ class Users(db.Model):
             'password' : self.password
         }
 
+class Cards(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(16), unique = True)
+    holderId = db.Column(db.Integer)
+    holderFirstName = db.Column(db.String(30))
+    holderLastName = db.Column(db.String(30))
+    securityCode = db.Column(db.String(3))
+    balance = db.Column(db.Double)
+    isVerified = db.Column(db.Boolean)
+
+    def __init__(self, number, holderId, holderFirstName, holderLastName, securityCode, balance, isVerified):
+        self.number = number
+        self.holderId = holderId
+        self.holderFirstName = holderFirstName
+        self.holderLastName = holderLastName
+        self.securityCode = securityCode
+        self.balance = balance
+        self.isVerified = isVerified
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'number' : self.number,
+            'holderId' : self.holderId,
+            'holderFirstName' : self.holderFirstName,
+            'holderLastName' : self.holderLastName,
+            'securityCode' : self.securityCode,
+            'balance' : self.balance,
+            'isVerified': self.isVerified,
+        }
+
 #Trazi usera po emailu i vraca ga na UI
 #login, 
 @app.route('/returnUser', methods=['GET', 'POST'])
@@ -126,6 +157,34 @@ def updateUser():
     else:
         return jsonify({'message': 'Greska, izmena nije uspela'}), 404
     
+#Za prosledjene podatke pravi novu karticu ukoliko se ime i prezime poklapa sa
+#imenom i prezimenom ulogovanog korisnika
+@app.route('/addCard', methods=['POST'])
+def addCard():
+    data = request.get_json()
+    recvNumber = data['number']
+    recvHolderId = data['holderId']
+    recvHolderFirstName = data['holderFirstName']
+    recvHolderLastName =  data['holderLastName']
+    recvSecurityCode = data['securityCode']
+    recvBalance = data['balance']
+
+    checkCard = Cards.query.filter_by(number = recvNumber).first()
+    checkUser = Users.query.filter_by(id = recvHolderId).first()
+
+    if checkUser is not None:
+        if checkUser.firstName == recvHolderFirstName and checkUser.lastName == recvHolderLastName:
+            if checkCard is None:
+                newCard = Cards(recvNumber,recvHolderId,recvHolderFirstName,recvHolderLastName,recvSecurityCode,recvBalance, False)
+                db.session.add(newCard)
+                db.session.commit()
+                return jsonify({'message':'Kartica poslata na verifikaciju!'}), 200
+            else:
+                return jsonify({'message':'Greska, kartica sa datim brojem je vec registrovana'}), 400
+        else:
+            return jsonify({'message':'Greska, ime i prezime na kartici se ne poklapa sa vasim imenom i prezimenom'}), 400
+    else:
+        return jsonify({'message':'Greska, korisnik kartice nije pronadjen u bazi'}),400
 
 
 if __name__ == '__main__':
